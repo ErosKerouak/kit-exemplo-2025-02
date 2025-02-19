ca23c705fc34c2e7f83dcf6269546623c91ce574
Eros Kerouak Cordeiro Pereira <eroskerouak@gmail.com>
2025-02-19 12:14:19 -0300

    Adiona script com o nome certo

---
+++ code/variacao_para_todos_os_paises.sh
@@ -0,0 +1,8 @@
+# Calcula a variação de temperatura para todos os paises
+
+echo "variacao_C_por_ano,pais"
+
+for arquivos in dados/temperatura/*.csv 
+do
+	python code/variacao_temperatura.py $arquivos
+done 
